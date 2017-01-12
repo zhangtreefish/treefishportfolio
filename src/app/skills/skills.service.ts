6a9044810b37438010cf807f6edb956bd0629a68
zhangtreefish <zhangtreefish@users.noreply.github.com>
2017-01-11 21:15:10 -0600

    Style

--- src/app/skills/skills.service.ts
+++ src/app/skills/skills.service.ts
@@ -2,10 +2,10 @@
 export class SkillService {
 	getSkills(): String[] {
 		return ["HTML5", "CSS3", "Bootstrap", "JavaScript", "TypeScript", "GitHub", "Gulp",
-		"Object-Oriented Programming", "jQuery", "Angular JS", "Angular2", "Angular2 CLI", "Typescript", "D3", "Knockout JS",
+		"Object-Oriented Programming", "jQuery", "Angular JS", "Angular2", "Typescript", "D3", "Knockout JS",
 		"web performance optimization", "Agile and scrum methodology",
 		"Unit Test with JasmineJS", "Selenium", "WebDriverIO", "Node.js", "Underscore.js",
-		 "Python", "SQLAlchemy", "Flask", "Jinja2", "OAuth 2.0", "Google App Engine",
-		  "Ubuntu linux server", "SQL", "Excel"];
+		 "Python", "SQLAlchemy", "Flask", "Jinja2", "OAuth 2.0", "Google App Engine ndb Client",
+		  "Ubuntu linux server", "SQL", "Excel", "MongoDB", "React", "Redux", "Webapp2"];
 	};
 }
